236bebabbd548e34ca5b3b3d0431aaf274accf9d
Aman Kumar Sahu <108629110+Coderaman12@users.noreply.github.com>
2024-08-29 14:03:13 +0530

    upto ball

--- main.py
+++ main.py
@@ -9,12 +9,17 @@ def main():
     # initialize tracker
     tracker = Tracker('models/best.pt')
     
-    tracks = tracker.get_object_tracker(video_frames,
+    tracks = tracker.get_object_tracks(video_frames,
                                         read_from_stub=True,
                                         stub_path='stubs/track_stubs.pkl')
     
+    #draw output
+    ## Draw object tracker
+    output_video_frames = tracker.draw_annotations(video_frames,tracks)
+    
+    
     #save video
-    save_video(video_frames,'output_videos/football_output.avi')
+    save_video(output_video_frames,'output_videos/football_output.avi')
 
 if __name__ == '__main__':
     main()
