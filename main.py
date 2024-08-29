6a0e5c1b9c70d49ad191b5ec88249f1a8b67d1e4
Aman Kumar Sahu <108629110+Coderaman12@users.noreply.github.com>
2024-08-29 14:55:39 +0530

    upto color of player shirt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
 from utils import read_video, save_video
 from trackers import Tracker
+import cv2
+
 def main():
     #read video
     video_frames = read_video("input_video/football.mp4")
@@ -12,6 +14,21 @@ def main():
     tracks = tracker.get_object_tracks(video_frames,
                                         read_from_stub=True,
                                         stub_path='stubs/track_stubs.pkl')
+    
+    # ---- saved cropped image of a player ----
+    # for track_id, player in tracks['players'][0].items():
+    #     bbox = player['bbox']
+    #     frame = video_frames[0]
+        
+    #     # crop box from frame
+    #     cropped_image = frame[int(bbox[1]):int(bbox[3]),int(bbox[0]):int(bbox[2])]
+        
+    #     # save the cropped image
+    #     cv2.imwrite(f'output_videos/cropped_image.jpg',cropped_image)
+    #     break
+
+    
+    # ---- this code is only used to cropped out an image from the video  ----
     
     #draw output
     ## Draw object tracker
